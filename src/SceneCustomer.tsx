4a21d28fbb197a1547e28dbda0c4479be2c11d73
Terry Yin <terry.yinzhe@gmail.com>
2023-04-17 18:20:04 +0800

    happy mom

--- src/SceneCustomer.tsx
+++ src/SceneCustomer.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Sequence} from 'remotion';
+import {Sequence, useCurrentFrame} from 'remotion';
 import { Company } from './parts/Company';
 import { CustomerGroup } from './parts/CustomerGroup';
 import { ValueArrow } from './parts/ValueArrow';
 
 export const SceneCustomer: React.FC = () => {
+  const frame = useCurrentFrame();
   return (
     <Sequence  durationInFrames={10 * 30}>
       <Company style={{position: 'absolute', left: '5%', top:'10%', width: '45%', height: '100%'}}/>
-      <CustomerGroup style={{position: 'absolute', left: '70%', top:'20%', width: '25%', height: '100%'}}/>
+      <CustomerGroup style={{position: 'absolute', left: '70%', top:'10%', width: '25%', height: '100%'}} happySince={60}/>
 			<div style={{position: 'absolute', left: '45%', top: '40%', width: '25%'}}>
-				<ValueArrow />
+				{frame > 15 && <ValueArrow />}
 			</div>
     </Sequence>
   );
--- src/parts/CustomerGroup.tsx
+++ src/parts/CustomerGroup.tsx
@@ -1,12 +1,22 @@
 import React, { CSSProperties } from 'react';
-import {Img, staticFile} from 'remotion';
+import {Img, interpolate, staticFile, useCurrentFrame} from 'remotion';
 
-export const CustomerGroup: React.FC<{style?: CSSProperties}> = ({ style }) => {
+export const CustomerGroup: React.FC<{style?: CSSProperties, happySince: number}> = ({ style, happySince }) => {
+	const frame = useCurrentFrame();
+  // Const { fps } = useVideoConfig();
+	const transitionDuration = 90;
+	const opacity = interpolate(frame, [happySince, happySince + transitionDuration], [0, 1], {
+    extrapolateLeft: 'clamp',
+    extrapolateRight: 'clamp',
+  });
+
+
   return (
       <div style={style}>
 				<div style={{position: 'relative', left: '0%', top:'0%', width: '100%', height: '100%'}}>
-					<Img src={staticFile("assets/WorriedMom.svg")} style={{position: 'absolute', left: '20%', top: '10%', width: '60%'}} />
-					<Img src={staticFile("assets/BabyUser.svg")} style={{position: 'absolute', left: '0%', top: '30%', width: '50%'}} />
+					<Img src={staticFile("assets/WorriedMom.svg")} style={{position: 'absolute', left: '20%', top: '15%', width: '80%', opacity: 1 - opacity }} />
+					<Img src={staticFile("assets/HappyMom.svg")} style={{position: 'absolute', left: '20%', top: '15%', width: '80%', opacity}} />
+					<Img src={staticFile("assets/BabyUser.svg")} style={{position: 'absolute', left: '0%', top: '25%', width: '50%'}} />
 				</div>
       </div>
   );
