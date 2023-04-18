90e5c2e6005dc37bd8a0add4b46c60056c116f6c
Terry Yin <terry.yinzhe@gmail.com>
2023-04-18 18:10:20 +0800

    improve title

--- src/hooks/useCurrentSubtitle.ts
+++ src/hooks/useCurrentSubtitle.ts
@@ -2,19 +2,22 @@ import { CurrentSubtitle } from 'src/models/CurrentSubtitle';
 import { Subtitle } from '../models/Subtitles';
 
 export const useCurrentSubtitle = (subtitles: Subtitle[], frame: number, fps: number): CurrentSubtitle => {
-  const currentSubtitle = subtitles.find(
-    (subtitle) =>
-      frame >= subtitle.startTime * fps && frame <= subtitle.endTime * fps
-  );
-
-  if (currentSubtitle === undefined) {
-    throw new Error('No subtitle found for current frame');
+  for (let i = 0; i < subtitles.length; i++) {
+    const subtitle = subtitles[i];
+    if (frame >= subtitle.startTime * fps) {
+      return {
+        globalFps: fps,
+        globalFrame: frame,
+        subtitle,
+        text: frame > subtitle.endTime * fps ? '' : subtitle.text,
+      };
+    }
   }
 
   return {
     globalFps: fps,
     globalFrame: frame,
-    subtitle: currentSubtitle,
-    text: currentSubtitle.text,
+    subtitle: subtitles[0],
+    text: ''
   }
 };
--- tests/hooks/useCurrentSubtitle.test.ts
+++ tests/hooks/useCurrentSubtitle.test.ts
@@ -3,35 +3,24 @@ import { useCurrentSubtitle } from 'src/hooks/useCurrentSubtitle';
 import { Subtitle } from 'src/models/Subtitles';
 
 describe('useCurrentSubtitle', () => {
+  const subtitles: Subtitle[] = [
+    { id: '1', startTime: 0, endTime: 2, text: 'First subtitle' },
+    { id: '2', startTime: 3, endTime: 4, text: 'Second subtitle' },
+    { id: '3', startTime: 4, endTime: 6, text: 'Third subtitle' },
+  ];
+
   it('should return the correct subtitle for the given frame', () => {
-    const subtitles: Subtitle[] = [
-      { id: 'aa', startTime: 0, endTime: 2, text: 'First subtitle' },
-      { id: 'aa', startTime: 2, endTime: 4, text: 'Second subtitle' },
-      { id: 'aa', startTime: 4, endTime: 6, text: 'Third subtitle' },
-    ];
+    const currentSubtitle = useCurrentSubtitle(subtitles, 60, 30);
 
-    const frame = 60;
-    const fps = 30;
-
-    const currentSubtitle = useCurrentSubtitle(subtitles, frame, fps);
-
-    expect(currentSubtitle.globalFps).toEqual(fps);
-    expect(currentSubtitle.globalFrame).toEqual(frame);
+    expect(currentSubtitle.globalFps).toEqual(30);
+    expect(currentSubtitle.globalFrame).toEqual(60);
     expect(currentSubtitle.subtitle).toEqual(subtitles[0]);
     expect(currentSubtitle.text).toEqual(subtitles[0].text);
   });
 
-  it('should throw an error if no subtitle is found for the current frame', () => {
-    const subtitles: Subtitle[] = [
-      { id: 'aa', startTime: 0, endTime: 1, text: 'First subtitle' },
-      { id: 'aa', startTime: 2, endTime: 3, text: 'Second subtitle' },
-    ];
-
-    const frame = 91;
-    const fps = 30;
-
-    expect(() => useCurrentSubtitle(subtitles, frame, fps)).toThrowError(
-      'No subtitle found for current frame'
-    );
+  it('should return the previous title if the next title havenot started yet', () => {
+    const currentSubtitle = useCurrentSubtitle(subtitles, 65, 30);
+    expect(currentSubtitle.subtitle.id).toEqual("1");
+    expect(currentSubtitle.text).toEqual("");
   });
 });
