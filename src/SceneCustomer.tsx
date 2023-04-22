c07a66f195081633db3b95991ac088e0b0c0d3e6
Terry Yin <terry.yinzhe@gmail.com>
2023-04-22 09:52:14 +0800

    switch to new story

--- src/SceneCustomer.tsx
+++ src/SceneCustomer.tsx
@@ -5,44 +5,30 @@ import autonomousComponent from './video_components/autonomousComponent';
 import { SceneLayout } from './SceneLayout';
 
 const subtitles: Subtitle[] = [
-	{ leadingBlank: 1, duration: 4, text: 'A company delivers services to users,', actions:[
-		{ actor: 'value to customer', actionType: 'appear', duration: 1 },
-	] },
-	{ leadingBlank: 0, duration: 4, text: 'aiming to solve customers\' problems', actions:[
-		{ actor: 'worried-mom', actionType: 'disappear', duration: 2 },
-		{ actor: 'happy-mom', actionType: 'appear', duration: 2 },
-	] },
-	{ leadingBlank: 0, duration: 4, text: 'or meet their needs.', actions:[
-		{ actor: 'happy-mom', actionType: 'glow', duration: 2 },
-	] },
-	{ leadingBlank: 1, duration: 4, text: 'In exchange, customers provide resources,', actions: [
-		{ actor: 'value from customer', actionType: 'appear', duration: 1 },
-	] },
-	{ leadingBlank: 0, duration: 4, text: 'such as money, to fuel growth.', actions:[
-		{ actor: 'company', actionType: 'glow', duration: 2 },
-	] },
-	{ leadingBlank: 1, duration: 3, text: 'Software products often play a', actions:[
-		{ actor: 'product', actionType: '3d rise', duration: 1 },
-		{ actor: 'product', actionType: '3d rotate', duration: 4, totalRotation: 360 + 30 },
-	] },
-	{ leadingBlank: 0, duration: 4, text: 'crucial role in providing services.', actions: [
+	{ leadingBlank: 1, duration: 5, text: "SpaceX is celebrating their rocket explosion,", actions: [
+			{ actor: "rocket", actionType: "appear", duration: 1 },
 	]},
-	{ leadingBlank: 1, duration: 3, text: 'Traditionally, businesses focused on' },
-	{ leadingBlank: 0, duration: 3, text: 'their core domain, not software.' },
-	{ leadingBlank: 1, duration: 3, text: 'Developers were part of external teams,', actions:[
-		{ actor: 'stage', actionType: 'scaleToUpperRight', duration: 1, outputRange: [100, 70] },
-	] },
-	{ leadingBlank: 1, duration: 3, text: 'hired to work on projects.' },
-	{ leadingBlank: 1, duration: 3, text: 'They delivered components to be' },
-	{ leadingBlank: 1, duration: 3, text: 'integrated with other parts later.' },
-	{ leadingBlank: 1, duration: 3, text: 'Now, businesses adopt a product-centric' },
-	{ leadingBlank: 1, duration: 3, text: 'strategy to grow their advantage.' },
-	{ leadingBlank: 1, duration: 3, text: 'This leads to the rise of' },
-	{ leadingBlank: 1, duration: 3, text: 'Product Developers.' },
-	{ leadingBlank: 1, duration: 3, text: 'Their work is customer-centric,' },
-	{ leadingBlank: 1, duration: 3, text: 'addressing real needs of users.' },
-	{ leadingBlank: 1, duration: 3, text: 'They focus on the entire product' },
-	{ leadingBlank: 1, duration: 3, text: 'and its seamless user experience.' },
+	{ leadingBlank: 0, duration: 5, text: "highlighting different attitudes towards failure in software development.", actions: [
+			{ actor: "software-dev", actionType: "appear", duration: 1 },
+	]},
+	{ leadingBlank: 1, duration: 6, text: "Learning from failures, like SpaceX's Starship test flight,", actions: [
+			{ actor: "rocket", actionType: "glow", duration: 2 },
+	]},
+	{ leadingBlank: 0, duration: 6, text: "is more valuable than merely experiencing them.", actions: []},
+	{ leadingBlank: 1, duration: 6, text: "Thomas Edison's light bulb story shows the importance of", actions: [
+			{ actor: "light-bulb", actionType: "appear", duration: 1 },
+	]},
+	{ leadingBlank: 0, duration: 6, text: "learning from each failure, not just failing repeatedly.", actions: []},
+	{ leadingBlank: 1, duration: 6, text: "For software developers, error messages contain crucial information.", actions: [
+			{ actor: "error-msg", actionType: "appear", duration: 1 },
+	]},
+	{ leadingBlank: 0, duration: 6, text: "Reading and understanding them guides you to success.", actions: [
+			{ actor: "success", actionType: "appear", duration: 1 },
+	]},
+	{ leadingBlank: 1, duration: 5, text: "Improving error messages benefits your peers.", actions: [
+			{ actor: "team", actionType: "appear", duration: 1 },
+	]},
+	{ leadingBlank: 0, duration: 4, text: "In conclusion, read the error message!", actions: []},
 ];
 
 export const SceneCustomer: React.FC = autonomousComponent(({frame, fps}) => {
