633af3b0fe28c609cbb3bb94d9ccb43a84730fd4
Terry Yin <terry.yinzhe@gmail.com>
2023-04-23 10:40:52 +0800

    Refactoring test

--- tests/video_conomponents/Subtitles.spec.tsx
+++ tests/video_conomponents/Subtitles.spec.tsx
@@ -5,22 +5,26 @@ import { Subtitles } from '@/video_components/Subtitles';
 import { AnimationContext } from '@/models/AnimationContext';
 import { AnimationContextProvider } from '@/hooks/useAnimationContext';
 
-const subtitle: Subtitle = 
-  { leadingBlank: 1, duration: 3, text: 'First subtitle.' };
-
-const animationContext: AnimationContext = {
-  allSubtitles: [subtitle],
-  globalFps: 30,
-  globalFrame: 60,
-};
-
 describe('Subtitles component', () => {
-  test('displays the correct subtitle text when there is an active subtitle', () => {
-    render(
+
+  const renderSubtitle = (subtitle: Subtitle) => {
+    const animationContext: AnimationContext = {
+      allSubtitles: [subtitle],
+      globalFps: 30,
+      globalFrame: 60,
+    };
+
+    const { container } = render(
       <AnimationContextProvider value={animationContext}>
         <Subtitles/>
       </AnimationContextProvider>
     );
+    return container;
+  }
+
+  test('displays the correct subtitle text when there is an active subtitle', () => {
+    renderSubtitle({ leadingBlank: 1, duration: 3, text: 'First subtitle.' });
     expect(screen.getByText('First subtitle.')).toBeInTheDocument();
   });
+
 });
