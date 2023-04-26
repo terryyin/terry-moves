2b64a69a2323b93c679092e962d6a10359b064f0
Terry Yin <terry.yinzhe@gmail.com>
2023-04-26 09:30:10 +0800

    new story

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,12 @@
 import { StoryProductDeveloper } from './StoryProductDeveloper';
 import { StoryFailureVsFailure } from './StoryFailureVsFailure';
+import { StoryBooleanParameters } from './StoryBooleanParameters';
 
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
 			<StoryFailureVsFailure />
+			<StoryBooleanParameters />
 			<StoryProductDeveloper />
 		</>
 	);
