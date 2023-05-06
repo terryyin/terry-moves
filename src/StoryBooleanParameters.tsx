c52104202db322157143de8ca45aa24c9e79f231
Terry Yin <terry.yinzhe@gmail.com>
2023-05-06 17:54:34 +0800

    bool

--- src/StoryBooleanParameters.tsx
+++ src/StoryBooleanParameters.tsx
@@ -5,7 +5,7 @@ import './global.css';
 
 import React, { CSSProperties } from 'react';
 
-import { Action, Subtitle } from './models/Subtitles';
+import { Subtitle } from './models/Subtitles';
 import { AbsoluteFill } from 'remotion';
 import AnimationEffect from './video_components/AnimationEffect';
 import { ThreeDFrame } from './video_components/ThreeDFrame';
@@ -25,27 +25,7 @@ import { Markdown } from './video_components/Markdown';
 import { Explosion } from './parts/Explosion';
 import { Subtitles } from './video_components/Subtitles';
 import { Anchor } from './video_components/Anchor';
-
-const fireActions: Action[] =
-[
-			  { actor: "blaster fire", actionType: "3d animation start", duration: 2, speed: 2 },
-			  { actor: "blaster assembly", actionType: "move and return", startDuration: 0.1, endDuration: 0.4, endingTimeAdjustment: 0.6, absolutePosition: [0.5, 0, 0]},
-];
-
-const loadedFireActions: Action[] =
-[
-				...fireActions,
-			  { actor: "blaster", actionType: "3d animation reverse", duration: 2, speed: 4, pauseAtEnd: true, freezeBeforeStart: true },
-			  { actor: "blaster temperature", actionType: "additive value change to", duration: 2, value: 0 },
-			  { actor: "blaster powerful fire", actionType: "3d animation start", duration: 3, speed: 2 },
-];
-
-const loadActions: Action[] =
-[
-			  { actor: "blaster", actionType: "3d animation start", duration: 1.2, speed: 3, pauseAtEnd: true, freezeBeforeStart: true },
-			  { actor: "blaster temperature", actionType: "additive value change to", duration: 1.2, value: 50 },
-			  { actor: "blaster assembly", actionType: "3d rotate and back", startDuration: 0.4, endDuration: 0.8, endingTimeAdjustment: 1.2,  totalRotation: [0, 0, -30]},
-];
+import { fireActions, loadActions, loadedFireActions } from './stories/booleans/common';
 
 export const booleanParametersSutitles: Subtitle[] = [
 	{
--- src/StoryBooleanReturns.tsx
+++ src/StoryBooleanReturns.tsx
@@ -16,13 +16,12 @@ import { Blaster } from './parts/Blaster';
 import { GroupInitialState } from './video_components/GroupInitialState';
 import { RocketPlume } from './parts/RocketPlume';
 import { CodeHighlight } from './video_components/CodeHighlight';
-import { CalloutCloud } from './video_components/CalloutCloud';
 import HealthBar from './video_components/HealthBar';
 import { LightSource } from './video_components/LightSource';
 import { Markdown } from './video_components/Markdown';
-import { Explosion } from './parts/Explosion';
 import { Subtitles } from './video_components/Subtitles';
 import { Anchor } from './video_components/Anchor';
+import { fireActions } from './stories/booleans/common';
 
 export const booleanReturnsSutitles: Subtitle[] = [
 		{ leadingBlank: 0, duration: 3, text: "Welcome back to the Oh My Bad Boolean Series!", actions:[
@@ -36,8 +35,15 @@ export const booleanReturnsSutitles: Subtitle[] = [
 		] },
 		{ leadingBlank: 1, duration: 5, text: "Oops, sorry, not 'my STYLE'. Should be 'Intention'.", actions:[
 				{ actor: "second title", actionType: "replace text", endingTimeAdjustment: 4, line: 1, match: "my STYLE", replacement: "intention", startDuration: 0, endDuration: 2 },
+				{ actor: "title", actionType: "disappear", startDuration: 1, offset: 5 },
 		] },
-		{ leadingBlank: 1, duration: 4, text: "You might recall the Blaster from my previous episode. You could fire it anytime, as long as you have ammo!", actions:[] },
+		{ leadingBlank: 1, duration: 3, text: "You might recall the Blaster from my previous episode.", actions:[
+		] },
+		{ leadingBlank: 1, duration: 4, text: "You could fire it anytime,", actions:[
+				...fireActions,
+		] },
+		{ leadingBlank: 0, duration: 4, text: "as long as you have ammo!", actions:[
+		] },
 		{ leadingBlank: 1, duration: 4, text: "The Blaster's fire function returned a boolean value, which was confusing. What did it mean? Shot successful? No ammo left?", actions:[] },
 		{ leadingBlank: 1, duration: 4, text: "This forced actions on callers, like writing a log. When failing to fire is a surprise, the function should throw an exception.", actions:[] },
 		{ leadingBlank: 1, duration: 4, text: "Avoiding forced actions on callers improves code readability. For checking ammo, add a separate hasAmmo function.", actions:[] },
@@ -110,9 +116,6 @@ export const StoryBooleanReturns: React.FC = () => {
 							color={0xffffff}
 						/>	
 						<LightSource actor="blaster temperature" position={[0, 0, 15]} color="#ff0000" />
-						<GroupInitialState rotation={[0, Math.PI * 3 / 2, 0]} position={[-5, 0, 0]} scale={0.15}>
-							<Explosion actor="blaster explosion" />
-						</GroupInitialState>
 						<GroupInitialState rotation={[0, Math.PI, 0]} position={[-3, 3.5, 0]} scale={1}>
             <ThreeAnimationEffect actor="blaster assembly" >
 								<Blaster actor="blaster"/>
@@ -126,17 +129,9 @@ export const StoryBooleanReturns: React.FC = () => {
           </ThreeDFrame>
       </AbsoluteFill>
 			</AnimationEffect>
-		<CodeHighlight actor="caller 1" codeString={caller1} style={{ left: '5%', top: '32%', width: '40%', height: '20%', }}>
-			<Anchor actor="a1-action" style={{left: "50%", top: "20px"}}/>
-			<Anchor actor="a1-param" style={{left: "78%", top: "35px"}}/>
-		</CodeHighlight>
 		<CodeHighlight actor="caller 2" codeString={caller2} style={{ left: '5%', top: '52%', width: '40%', height: '20%', }}>
 			<Anchor actor="a2-action" style={{left: "50%", top: "20px"}}/>
 			<Anchor actor="a2-param" style={{left: "78%", top: "35px"}}/>
-		</CodeHighlight>
-		<CodeHighlight actor="caller 3" codeString={caller3} style={{ left: '5%', top: '74%', width: '40%', height: '20%', }}>
-			<Anchor actor="a3-action" style={{left: "50%", top: "20px"}}/>
-			<Anchor actor="a3-param" style={{left: "78%", top: "35px"}}/>
 		</CodeHighlight>
 		<CodeHighlight actor="callee" codeString={codeString} style={{ left: '55%', top: '30%', width: '40%', height: '50%', }}>
 			<Anchor actor="callee-action" style={{left: "15px", top: "25px"}}/>
@@ -144,30 +139,6 @@ export const StoryBooleanReturns: React.FC = () => {
 			<Anchor actor="callee-fire" style={{left: "60px", top: "125px"}}/>
 			<Anchor actor="callee-loadedfire" style={{left: "15px", top: "120px"}}/>
 		</CodeHighlight>
-
-		<CalloutCloud actor='caller 1 callout1' style={{top: '10%', left: "2%"}} tailShift={-70} tailHeightPx={100}>
-			<span style={{ fontSize: '30px', margin: 0 }} > 🤔 Can we pass the load amount to action()? 💡  </span>
-		</CalloutCloud>
-		<CalloutCloud actor='caller 1 callout2' style={{top: '25%', left: "10%"}} tailShift={-70} tailHeightPx={20}>
-			<span style={{ fontSize: '30px', margin: 0 }} > 🎉🎉🎉 Yeah! Ticket closed! 🎉🎉🎉 </span>
-		</CalloutCloud>
-		<CalloutCloud actor='callee callout1' style={{top: '21%', left: "20%"}} tailShift={20}>
-			<span style={{ fontSize: '30px', margin: 0 }} > Absolutely! Let's update the interface. 🛠️  </span>
-		</CalloutCloud>
-		<CalloutCloud actor='caller 3 callout1' style={{top: '63%', left: "5%"}} tailShift={-20}>
-			<span style={{ fontSize: '30px', margin: 0 }} > Woohoo! 🎉 Just what I needed! </span>
-		</CalloutCloud>
-		<CalloutCloud actor='caller 3 callout2' style={{top: '63%', left: "30%"}} tailShift={-20}>
-			<span style={{ fontSize: '30px', margin: 0, backgroundColor: "#ffaaaa"}} >Oops. SORRY!!!!!!!! </span>
-		</CalloutCloud>
-		<CalloutCloud actor='caller 2 callout1' style={{top: '41%', left: "4%"}} tailShift={-40}>
-			<span style={{ fontSize: '30px', margin: 0 , backgroundColor: "#ffaaaa"}} > 😠 What?! Why change it? What amount? 🚫  </span>
-		</CalloutCloud>
-		<CalloutCloud actor='teammate callout' style={{top: '13%', left: "calc(100% - 180px)"}} tailShift={110}>
-			<span style={{ fontSize: '30px', margin: 0 , backgroundColor: "#ffaaaa"}} > Ouch! Hey!  </span>
-		</CalloutCloud>
-
-    <AnimationEffect actor="mask" style={{backgroundColor: "rgba(0, 0, 0, 0.7)"}}/>
 
     <Markdown actor="learning from tight coupling" style={announceBoardStyle}
 			md={learningCoupling}
