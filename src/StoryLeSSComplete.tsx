a2a420f476e6fda5e02d60babfa522a428099bc5
Terry Yin <terry.yinzhe@gmail.com>
2023-05-10 18:50:47 +0800

    head

--- src/StoryLeSSComplete.tsx
+++ src/StoryLeSSComplete.tsx
@@ -8,19 +8,19 @@ import { Story } from './video_components/Story';
 import { TwoDImage } from './video_components/TwoDImage';
 import { ThreeDArrow } from './video_components/ThreeDArrow';
 
-export const booleanReturnsSutitles: Subtitle[] = [
-		{ leadingBlank: 1, duration: 3, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
+export const lessCompletePictureSubtitles: Subtitle[] = [
+		{ leadingBlank: 1, duration: 3, text: "LeSS provides a minimal barely sufficient core: the LeSS rules. And on top of that it encourages experimentation influenced by the LeSS principles.", actions: [
 			{ actor: "camera", actionType: "move", duration: 3, absolutePosition: [0, 0, -8] },
 			{ actor: "arrow", actionType: "additive value change to", duration: 0, value: 0},
 		]},
-		{ leadingBlank: 2, duration: 3, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
+		{ leadingBlank: 2, duration: 3, text: "It has rules that provide a minimum core structure.", actions: [
 			{ actor: "camera", actionType: "move", duration: 3, absolutePosition: [-6, 0, -3] },
 			{ actor: "camera", actionType: "camera look at", duration: 3, absolutePosition: [0, 0, 3] },
 		]},
-		{ leadingBlank: 1, duration: 3, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
+		{ leadingBlank: 1, duration: 3, text: "Principles that guide experimentation and adoption.", actions: [
 			{ actor: "arrow", actionType: "additive value change to", duration: 3, value: 1},
 		]},
-		{ leadingBlank: 2, duration: 5, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
+		{ leadingBlank: 2, duration: 5, text: "And this is called a 'LeSS Complete Picture'", actions: [
 			{ actor: "camera", actionType: "move", duration: 3, absolutePosition: [0, 0, 0] },
 			{ actor: "camera", actionType: "camera look at", duration: 3, absolutePosition: [0, 0, 0] },
 		]},
@@ -29,8 +29,8 @@ export const booleanReturnsSutitles: Subtitle[] = [
 
 export const StoryLeSSComplete: React.FC = () => {
   return (
-		<Story id="StoryLeSSComplete" width={720} height={720} subtitles={booleanReturnsSutitles}  >
-    <AbsoluteFill style={{}}>
+		<Story id="StoryLeSSComplete" width={720} height={720} subtitles={lessCompletePictureSubtitles}  >
+    <AbsoluteFill style={{backgroundColor: "beige"}}>
 			<ThreeDFrame cameraDistance={4} lookAtY={0} lookAtZ={-6} cameraY={0}>
 				<directionalLight castShadow position={[10, 20, 15]} intensity={10} color={0xffffff} />	
 				<TwoDImage actor="experiments" rotation={[0, 0, 0]} position={[0, 0, 0]} scale={1} url={staticFile("assets/less/complete/complete-experiments.png")} />
