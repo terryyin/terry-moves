ab289d4305ba0c28c71de0cce7b6bd9feb218bfd
Terry Yin <terry.yinzhe@gmail.com>
2023-05-10 15:10:25 +0800

    complete

--- src/StoryLeSSComplete.tsx
+++ src/StoryLeSSComplete.tsx
@@ -6,68 +6,25 @@ import { AbsoluteFill, staticFile } from 'remotion';
 import { ThreeDFrame } from './video_components/ThreeDFrame';
 import { Story } from './video_components/Story';
 import { TwoDImage } from './video_components/TwoDImage';
-import * as THREE from 'three';
-import { useFrame } from '@react-three/fiber';
+import { ThreeDArrow } from './video_components/ThreeDArrow';
 
 export const booleanReturnsSutitles: Subtitle[] = [
-		{ leadingBlank: 0, duration: 3, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
+		{ leadingBlank: 1, duration: 3, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
 			{ actor: "camera", actionType: "move", duration: 3, absolutePosition: [0, 0, -8] },
+			{ actor: "arrow", actionType: "additive value change to", duration: 0, value: 0},
 		]},
 		{ leadingBlank: 2, duration: 3, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
 			{ actor: "camera", actionType: "move", duration: 3, absolutePosition: [-6, 0, -3] },
 			{ actor: "camera", actionType: "camera look at", duration: 3, absolutePosition: [0, 0, 3] },
-		]}
+		]},
+		{ leadingBlank: 1, duration: 3, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
+			{ actor: "arrow", actionType: "additive value change to", duration: 3, value: 1},
+		]},
+		{ leadingBlank: 2, duration: 5, text: "Welcome back to the Oh My Bad Boolean Series!", actions: [
+			{ actor: "camera", actionType: "move", duration: 3, absolutePosition: [0, 0, 0] },
+			{ actor: "camera", actionType: "camera look at", duration: 3, absolutePosition: [0, 0, 0] },
+		]},
 ];
-
-
-
-const StaticLineAndArrow: React.FC = () => {
-
-	const startPoint = new THREE.Vector3(0.3, -0.5, -6);
-  const endPoint = new THREE.Vector3(0.7, -2.3, 0.3);
-
-  // Create a curved line
-  const curve = new THREE.CatmullRomCurve3([startPoint, new THREE.Vector3(1.3, -1, 1), new THREE.Vector3(1, -2.4, 0.5), endPoint]);
-  const tubeRadius = 0.1;
-  const tubeGeometry = new THREE.TubeGeometry(curve, 50, tubeRadius, 8, false);
-  const tubeMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-
-
-  // Calculate arrow properties
-  const arrowDirection = curve.getTangentAt(1); // Get the tangent at the end of the curve
-  const arrowLength = startPoint.distanceTo(endPoint) * 0.1;
-  const arrowColor = new THREE.Color(0xff0000);
-
-  // Create arrowhead geometry (cone)
-  const arrowHeadRadius = tubeRadius * 2;
-  const arrowHeadHeight = arrowLength;
-  const arrowHeadGeometry = new THREE.ConeGeometry(arrowHeadRadius, arrowHeadHeight, 32);
-
-  // Calculate arrowhead position and orientation
-  const arrowHeadPosition = endPoint.clone().add(arrowDirection.clone().multiplyScalar(arrowHeadHeight / 2));
-  const arrowHead = useRef<THREE.Mesh>(null);
-
-	useFrame(() => {
-		if (arrowHead.current) {
-      arrowHead.current.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), arrowDirection.clone().normalize());
-    }
-  });
-	
-  return (
-		<>
-		<mesh geometry={tubeGeometry} material={tubeMaterial} />
-			<mesh
-        ref={arrowHead}
-        geometry={arrowHeadGeometry}
-        material={new THREE.MeshBasicMaterial({ color: arrowColor })}
-        position={arrowHeadPosition}
-      />
-
-		</>
-  );
-};
-
-export default StaticLineAndArrow;
 
 
 export const StoryLeSSComplete: React.FC = () => {
@@ -80,7 +37,7 @@ export const StoryLeSSComplete: React.FC = () => {
 				<TwoDImage actor="guides" rotation={[0, 0, 0]} position={[0, 0, -2]} scale={1.5} url={staticFile("assets/less/complete/complete-guides.png")} />
 				<TwoDImage actor="rules" rotation={[0, 0, 0]} position={[0, 0, -4]} scale={1.98} url={staticFile("assets/less/complete/complete-rules.png")} />
 				<TwoDImage actor="principles" rotation={[0, 0, 0]} position={[0, 0, -6]} scale={2.4} url={staticFile("assets/less/complete/complete-principles.png")} />
-				<StaticLineAndArrow />
+				<ThreeDArrow actor="arrow" />
 			</ThreeDFrame>
 		</AbsoluteFill>
 		</Story>
---
+++ src/video_components/ThreeDArrow.tsx
@@ -0,0 +1,106 @@
+import {useRef} from 'react'
+import React  from 'react';
+
+import * as THREE from 'three';
+import { useFrame } from '@react-three/fiber';
+import { useAnimationContext } from '../hooks/useAnimationContext';
+
+function createPartialTubeGeometry(curve: THREE.CatmullRomCurve3, percentage: number, radialSegments = 8, tubularSegments = 50, radius = 0.1) {
+  const geometry = new THREE.BufferGeometry();
+  const vertices = [];
+  const indices = [];
+  const normals = [];
+  const uvs = [];
+
+  const segmentPercentage = Math.max(Math.min(percentage, 1), 0);
+  const segmentCount = Math.round(tubularSegments * segmentPercentage);
+
+  for (let i = 0; i <= segmentCount; i++) {
+    const u = i / tubularSegments;
+    const p1 = curve.getPointAt(u);
+    const p2 = curve.getTangentAt(u).normalize().cross(new THREE.Vector3(0, 1, 0)).multiplyScalar(radius);
+
+    for (let j = 0; j <= radialSegments; j++) {
+      const v = j / radialSegments;
+      const vertex = p1.clone().add(p2.clone().applyAxisAngle(curve.getTangentAt(u), (v * Math.PI * 2)));
+      vertices.push(vertex.x, vertex.y, vertex.z);
+      normals.push(p2.x, p2.y, p2.z);
+      uvs.push(i / tubularSegments, j / radialSegments);
+    }
+  }
+
+  for (let i = 0; i < segmentCount; i++) {
+    for (let j = 0; j < radialSegments; j++) {
+      const a = i * (radialSegments + 1) + j;
+      const b = a + radialSegments + 1;
+      indices.push(a, b, a + 1);
+      indices.push(b, b + 1, a + 1);
+    }
+  }
+
+  geometry.setIndex(indices);
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+  geometry.setAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
+  geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
+
+  return geometry;
+}
+
+
+export const ThreeDArrow: React.FC<{actor: string}> = ({actor}) => {
+  const percentage = useAnimationContext().getGeneralValue(actor) ?? 0;
+
+	const startPoint = new THREE.Vector3(0.3, -0.5, -6);
+  const endPoint = new THREE.Vector3(0.8, -2.0, 0.3);
+
+  // Create a curved line
+  const curve = new THREE.CatmullRomCurve3([startPoint, new THREE.Vector3(1.3, -1, 1), new THREE.Vector3(1, -2.4, 0.5), endPoint]);
+  const tubeRadius = 0.1;
+  // Const tubeGeometry = new THREE.TubeGeometry(curve, 50, tubeRadius, 8, false);
+	const tubeGeometry = createPartialTubeGeometry(curve, percentage, 8, 50, tubeRadius);
+
+  const tubeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+
+
+  // Calculate arrow properties
+  const arrowLength = 0.4;
+  const arrowColor = new THREE.Color(0x000000);
+
+  // Create arrowhead geometry (cone)
+  const arrowHeadRadius = tubeRadius * 2;
+  const arrowHeadHeight = arrowLength;
+  const arrowHeadGeometry = new THREE.ConeGeometry(arrowHeadRadius, arrowHeadHeight, 32);
+
+  // Calculate arrowhead position and orientation
+  const arrowHead = useRef<THREE.Mesh>(null);
+	const mesh = useRef<THREE.Mesh>(null);
+
+	useFrame(() => {
+    if (mesh.current) {
+			const newGeometry = createPartialTubeGeometry(curve, percentage, 8, 50, 0.1);
+			mesh.current.geometry.dispose();
+			mesh.current.geometry = newGeometry;
+
+			// Update arrowhead position and direction
+			if (arrowHead.current) {
+				const arrowPosition = curve.getPointAt(percentage);
+				const arrowDirection = curve.getTangentAt(percentage);
+
+				arrowHead.current.position.copy(arrowPosition);
+				arrowHead.current.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), arrowDirection.clone().normalize());
+			}
+		}
+  });
+	
+  return (
+		<>
+		<mesh ref={mesh} geometry={tubeGeometry} material={tubeMaterial} />
+			(percentage !== 0 && <mesh
+        ref={arrowHead}
+        geometry={arrowHeadGeometry}
+        material={new THREE.MeshBasicMaterial({ color: arrowColor })}
+      />)
+
+		</>
+  );
+};
