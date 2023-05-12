237cc8a9e958f3699fd90e304c778694de01c7fd
Terry Yin <terry.yinzhe@gmail.com>
2023-05-12 12:31:06 +0800

    story

--- src/StoryWhyWhyLeSS.tsx
+++ src/StoryWhyWhyLeSS.tsx
@@ -1,19 +1,30 @@
 
 import { Subtitle } from './models/Subtitles';
-import { AbsoluteFill } from 'remotion';
+import { AbsoluteFill, Img, staticFile } from 'remotion';
 import { ThreeDFrame } from './video_components/ThreeDFrame';
 import { Story } from './video_components/Story';
 import { Scrum } from './parts/Scrum';
 import { OddeLogo } from './parts/OddeLogo';
 import { OddeLogoInner } from './parts/OddeLogoInner';
 import { FlipCoin } from './video_components/AutonomousComponents/FlipCoin';
+import { Subtitles } from './video_components/Subtitles';
+import AnimationEffect from './video_components/AnimationEffect';
 
 export const whyWhyLeSSSubtitles: Subtitle[] = [
-{ leadingBlank: 1, duration: 5, text: 'The creators and trainers of Large-Scale Scrum have subtly updated the "Why LeSS?" article.', actions: []},
+{ leadingBlank: 1, duration: 5, text: 'The creators and trainers of Large-Scale Scrum have subtly updated the "Why LeSS?" article.', actions: [
+	{ actor: "overview", actionType: "appear", endingTimeAdjustment: 0, endDuration: 2,  persistUntilSubtitleId: "letsdelve"},
+	{ actor: "overview", actionType: "scale", duration: 4, outputRange: [1, 6]},
+	{ actor: "overview", actionType: "move", duration: 4, absolutePosition: [0, 190, 0]},
+]},
 { leadingBlank: 1, duration: 5, text: 'This update shifts focus from scaling Scrum to achieving organizational adaptiveness.', actions: []},
-{ leadingBlank: 1, duration: 5, text: `Let's delve into these changes.`, actions: []},
-{ leadingBlank: 1, duration: 5, text: '"Why LeSS?" was penned by Bas and Craig as the foundational reasoning behind LeSS.', actions: []},
-{ leadingBlank: 1, duration: 5, text: 'The earlier version of the article depicted LeSS as a framework to scale Scrum, keeping its essence intact.', actions: []},
+{ id: "letsdelve", leadingBlank: 1, duration: 3, text: `Let's delve into these changes.`, actions: [
+]},
+{ leadingBlank: 1, duration: 5, text: '"Why LeSS?" was penned by Bas and Craig as the foundational reasoning behind LeSS.', actions: [
+	{ actor: "page", actionType: "rotate and rise", duration: 2, value: 100},
+]},
+{ leadingBlank: 1, duration: 5, text: 'The earlier version of the article depicted LeSS as a framework to scale Scrum, keeping its essence intact.', actions: [
+	{ actor: "page", actionType: "move", duration: 2, absolutePosition: [800, 0, 0]},
+]},
 { leadingBlank: 1, duration: 5, text: `While this depiction still holds true, the term 'Scrum' has grown increasingly nebulous.`, actions: []},
 { leadingBlank: 1, duration: 5, text: 'The updated version underscores the essence of LeSS - prioritizing systems thinking, simpler structures, and continuous improvement for building adaptable organizations.', actions: []},
 { leadingBlank: 1, duration: 5, text: 'This essence is embodied in the principles of LeSS.', actions: []},
@@ -55,15 +66,24 @@ export const StoryWhyWhyLeSS: React.FC = () => {
 				<Scrum actor="scrum2" headActor='scrum head' bendingActor="bending2"/>
 				<Scrum actor="scrum3" headActor='scrum head' bendingActor="bending3"/>
 			</ThreeDFrame>
-		<AbsoluteFill style={{ left: '83%', top: '4.6%', width: '10%', height: '20%'}}>
-			<span style={{fontSize: '20px', fontFamily: "Poppins, sans serif"}}>terry@</span>
-		</AbsoluteFill>
-    <AbsoluteFill style={{ left: '90%', top: '2%', width: '10%', height: '20%'}}>
-			<OddeLogo />
-			<FlipCoin speed={2} interval={4} shift={1} >
-			  <OddeLogoInner />
-			</FlipCoin>
-		</AbsoluteFill>
+			<AnimationEffect actor="overview" style={{ width: "100%", top: "30%" }}>
+				<Img src={staticFile('assets/less/LeSS-overview-diagram.png')} style={{ width: "100%" }}/>
+			</AnimationEffect>
+			<AnimationEffect actor="page" style={{ width: "80%", left: "10%", top: "10%" }}>
+				<Img src={staticFile('assets/less/why-less-page.png')} style={{ width: "100%" }}/>
+			</AnimationEffect>
+			<AbsoluteFill style={{ left: '83%', top: '4.6%', width: '10%', height: '20%'}}>
+				<span style={{fontSize: '20px', fontFamily: "Poppins, sans serif"}}>terry@</span>
+			</AbsoluteFill>
+			<AbsoluteFill style={{ left: '90%', top: '2%', width: '10%', height: '20%'}}>
+				<OddeLogo />
+				<FlipCoin speed={2} interval={4} shift={1} >
+					<OddeLogoInner />
+				</FlipCoin>
+			</AbsoluteFill>
+			<AnimationEffect actor="subtitles">
+				<Subtitles scale={1} language="zhTW"/>
+			</AnimationEffect>
 		</AbsoluteFill>
 		</Story>
   );
