f2d1996631073a7d5b1f642887c8ad46ad60dc41
Terry Yin <terry.yinzhe@gmail.com>
2023-05-12 09:14:23 +0800

    scrum

--- src/StoryScalingScrum.tsx
+++ src/StoryScalingScrum.tsx
@@ -9,8 +9,15 @@ export const scalingScrumSubtitles: Subtitle[] = [
 		{ leadingBlank: 0, duration: 4, text: "Principles that guide experimentation and adoption.", actions: [
 			{ actor: "scrum1", actionType: "additive value change to", duration: 4, value: 1},
 		]},
+		{ leadingBlank: 0, duration: 5, text: "Principles that guide experimentation and adoption.", actions: [
+			{ actor: "scrum2", actionType: "additive value change to", duration: 0.1, value: 1},
+			{ actor: "scrum3", actionType: "additive value change to", duration: 0.1, value: 1},
+			{ actor: "bending1", actionType: "additive value change to", duration: 3, value: 0},
+			{ actor: "bending2", actionType: "additive value change to", duration: 3, value: 0.44},
+			{ actor: "bending3", actionType: "additive value change to", duration: 3, value: 0.88},
+		]},
 		{ leadingBlank: 0, duration: 4, text: "Principles that guide experimentation and adoption.", actions: [
-			{ actor: "scrum head", actionType: "additive value change to", duration: 4, value: 0.3},
+			{ actor: "scrum head", actionType: "additive value change to", duration: 2, value: 0.4},
 		]},
 ];
 
@@ -18,12 +25,12 @@ export const StoryScalingScrum: React.FC = () => {
   return (
 		<Story id="StoryScalingScrum" width={720} height={720} subtitles={scalingScrumSubtitles}  >
     <AbsoluteFill style={{backgroundColor: "beige"}}>
-			<ThreeDFrame cameraDistance={5} lookAtY={0} lookAtZ={-6} cameraY={-1}>
+			<ThreeDFrame cameraDistance={6} lookAtY={0} lookAtZ={0} cameraY={-1}>
 				<pointLight position={[10, 10, 10]} />
 				<directionalLight castShadow position={[10, 20, 15]} intensity={.9} color={0xffffff} />	
-				<Scrum actor="scrum1" headActor='scrum head'/>
-				<Scrum actor="scrum2" headActor='scrum head'/>
-				<Scrum actor="scrum3" headActor='scrum head'/>
+				<Scrum actor="scrum1" headActor='scrum head' bendingActor='bending1'/>
+				<Scrum actor="scrum2" headActor='scrum head' bendingActor="bending2"/>
+				<Scrum actor="scrum3" headActor='scrum head' bendingActor="bending3"/>
 			</ThreeDFrame>
 		</AbsoluteFill>
 		</Story>
--- src/parts/Scrum.tsx
+++ src/parts/Scrum.tsx
@@ -3,20 +3,26 @@ import React  from 'react';
 import * as THREE from 'three';
 import { useAnimationContext } from '../hooks/useAnimationContext';
 import { ThreeDArrow } from '../video_components/ThreeDArrow';
+import { GroupInitialState } from '../video_components/GroupInitialState';
 
 
+// eslint-disable-next-line max-params
 function generateSpiralCirclePoints(
   circleRadius: number,
   zIncrement: number,
   numberOfPoints: number,
-  numberOfTurns: number
+  numberOfTurns: number,
+  bending: number,
 ): THREE.Vector3[] {
   const points: THREE.Vector3[] = [];
 
+  const startAngle = bending / 1.5;
+  const trueNumberOfTurns = numberOfTurns - startAngle * numberOfTurns / Math.PI ;
+
   for (let i = 0; i <= numberOfPoints; i++) {
-    const angle = (2 * Math.PI * numberOfTurns * i) / numberOfPoints;
+    const angle = ((2 * Math.PI + startAngle) * trueNumberOfTurns * i ) / numberOfPoints;
     const x = circleRadius * Math.sin(angle);
-    const y = -circleRadius * Math.cos(angle);
+    const y = -circleRadius * Math.cos(angle) + bending;
     const z = (zIncrement * i) / numberOfPoints;
 
     points.push(new THREE.Vector3(x, y, z));
@@ -25,20 +31,37 @@ function generateSpiralCirclePoints(
   return points;
 }
 
-export const Scrum: React.FC<{actor: string, headActor: string}> = ({actor, headActor}) => {
+export const Scrum: React.FC<{actor: string, headActor: string, bendingActor: string}> = ({actor, headActor, bendingActor}) => {
   const headMove = useAnimationContext().getGeneralValue(headActor) ?? 0;
+  const trueBending = useAnimationContext().getGeneralValue(bendingActor) ?? 0;
 
   const radius = 1;
   const spiralCirclePoints = generateSpiralCirclePoints(
     radius,
     1,
     100,
-    1
+    1,
+    trueBending
   );
 
 
   const oneSprint = [new THREE.Vector3(-2.4, -radius, -1), ...spiralCirclePoints, new THREE.Vector3(1, -radius, 1.2), new THREE.Vector3(2, -radius + headMove, 1.4)]
+
+  const daily = generateSpiralCirclePoints(
+    0.3,
+    0,
+    100,
+    1,
+    1.5
+  );
+
+
   return (
-    <ThreeDArrow actor={actor} tubeRadius={0.2} color={new THREE.Color(0x850000)} points={oneSprint}/>
+    <>
+    <ThreeDArrow actor={actor} tubeRadius={0.2} color={new THREE.Color(0x59788e)} points={oneSprint}/>
+    <GroupInitialState position={[1, 0 + trueBending, 0]} rotation={[0, 0, 0]} scale={1}>
+      <ThreeDArrow actor={actor} tubeRadius={0.1} color={new THREE.Color(0xed7117)} points={daily}/>
+    </GroupInitialState>
+    </>
   );
 };
