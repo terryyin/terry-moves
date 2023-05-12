d66bcaf7b30b89bac53b258923299c1f94272e16
Terry Yin <terry.yinzhe@gmail.com>
2023-05-12 13:51:09 +0800

    d

--- src/StoryScalingScrum.tsx
+++ src/StoryScalingScrum.tsx
@@ -3,21 +3,17 @@ import { Subtitle } from './models/Subtitles';
 import { AbsoluteFill } from 'remotion';
 import { ThreeDFrame } from './video_components/ThreeDFrame';
 import { Story } from './video_components/Story';
-import { Scrum } from './parts/Scrum';
+import { ScalableScrum, empiricalActions, growScrum1Actions, scaleScrumActions } from './stories/scrum';
 
 export const scalingScrumSubtitles: Subtitle[] = [
 		{ leadingBlank: 0, duration: 4, text: "Principles that guide experimentation and adoption.", actions: [
-			{ actor: "scrum1", actionType: "additive value change to", duration: 4, value: 1},
+			...growScrum1Actions,
 		]},
 		{ leadingBlank: 0, duration: 5, text: "Principles that guide experimentation and adoption.", actions: [
-			{ actor: "scrum2", actionType: "additive value change to", duration: 0.1, value: 1},
-			{ actor: "scrum3", actionType: "additive value change to", duration: 0.1, value: 1},
-			{ actor: "bending1", actionType: "additive value change to", duration: 3, value: 0},
-			{ actor: "bending2", actionType: "additive value change to", duration: 3, value: 0.44},
-			{ actor: "bending3", actionType: "additive value change to", duration: 3, value: 0.88},
+			...scaleScrumActions,
 		]},
 		{ leadingBlank: 0, duration: 4, text: "Principles that guide experimentation and adoption.", actions: [
-			{ actor: "scrum head", actionType: "additive value change to", duration: 2, value: 0.4},
+			...empiricalActions,
 		]},
 ];
 
@@ -28,9 +24,7 @@ export const StoryScalingScrum: React.FC = () => {
 			<ThreeDFrame cameraDistance={6} lookAtY={0} lookAtZ={0} cameraY={-1}>
 				<pointLight position={[10, 10, 10]} />
 				<directionalLight castShadow position={[10, 20, 15]} intensity={.9} color={0xffffff} />	
-				<Scrum actor="scrum1" headActor='scrum head' bendingActor='bending1'/>
-				<Scrum actor="scrum2" headActor='scrum head' bendingActor="bending2"/>
-				<Scrum actor="scrum3" headActor='scrum head' bendingActor="bending3"/>
+				<ScalableScrum />
 			</ThreeDFrame>
 		</AbsoluteFill>
 		</Story>
--- src/StoryWhyWhyLeSS.tsx
+++ src/StoryWhyWhyLeSS.tsx
@@ -3,12 +3,13 @@ import { Subtitle } from './models/Subtitles';
 import { AbsoluteFill, Img, staticFile } from 'remotion';
 import { ThreeDFrame } from './video_components/ThreeDFrame';
 import { Story } from './video_components/Story';
-import { Scrum } from './parts/Scrum';
 import { OddeLogo } from './parts/OddeLogo';
 import { OddeLogoInner } from './parts/OddeLogoInner';
 import { FlipCoin } from './video_components/AutonomousComponents/FlipCoin';
 import { Subtitles } from './video_components/Subtitles';
 import AnimationEffect from './video_components/AnimationEffect';
+import { ScalableScrum, empiricalActions, growScrum1Actions, scaleScrumActions } from './stories/scrum';
+import { Anchor } from './video_components/Anchor';
 
 export const whyWhyLeSSSubtitles: Subtitle[] = [
 { leadingBlank: 1, duration: 5, text: 'The creators and trainers of Large-Scale Scrum have subtly updated the "Why LeSS?" article.', actions: [
@@ -22,8 +23,16 @@ export const whyWhyLeSSSubtitles: Subtitle[] = [
 { leadingBlank: 1, duration: 5, text: '"Why LeSS?" was penned by Bas and Craig as the foundational reasoning behind LeSS.', actions: [
 	{ actor: "page", actionType: "rotate and rise", duration: 2, value: 100},
 ]},
-{ leadingBlank: 1, duration: 5, text: 'The earlier version of the article depicted LeSS as a framework to scale Scrum, keeping its essence intact.', actions: [
+{ leadingBlank: 1, duration: 5, text: 'The earlier version of the article emphosized that Large-Scale Scrum is Scrum.', actions: [
 	{ actor: "page", actionType: "move", duration: 2, absolutePosition: [800, 0, 0]},
+	...growScrum1Actions,
+]},
+{ leadingBlank: 1, duration: 4, text: 'It depicted LeSS as a framework to scale Scrum,', actions: [
+	...scaleScrumActions,
+]},
+{ leadingBlank: 0, duration: 4, text: 'keeping its essence intact.', actions: [
+	{ actor: "center", actionType: "connect to",  startDuration: 1, target: "adjust-direction", bentLevel: -100, endingTimeAdjustment: 3},
+	...empiricalActions,
 ]},
 { leadingBlank: 1, duration: 5, text: `While this depiction still holds true, the term 'Scrum' has grown increasingly nebulous.`, actions: []},
 { leadingBlank: 1, duration: 5, text: 'The updated version underscores the essence of LeSS - prioritizing systems thinking, simpler structures, and continuous improvement for building adaptable organizations.', actions: []},
@@ -59,12 +68,12 @@ export const StoryWhyWhyLeSS: React.FC = () => {
 		<Story id="StoryWhyWhyLeSS" width={720} height={720} subtitles={whyWhyLeSSSubtitles}  >
 		<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'/>
     <AbsoluteFill style={{backgroundColor: "beige"}}>
+			<Anchor actor="center" style={{left: "50%", top: "45%"}}/>
+			<Anchor actor="adjust-direction" style={{left: "70%", top: "50%"}}/>
 			<ThreeDFrame cameraDistance={6} lookAtY={0} lookAtZ={0} cameraY={-1}>
 				<pointLight position={[10, 10, 10]} />
 				<directionalLight castShadow position={[10, 20, 15]} intensity={.9} color={0xffffff} />	
-				<Scrum actor="scrum1" headActor='scrum head' bendingActor='bending1'/>
-				<Scrum actor="scrum2" headActor='scrum head' bendingActor="bending2"/>
-				<Scrum actor="scrum3" headActor='scrum head' bendingActor="bending3"/>
+				<ScalableScrum />
 			</ThreeDFrame>
 			<AnimationEffect actor="overview" style={{ width: "100%", top: "30%" }}>
 				<Img src={staticFile('assets/less/LeSS-overview-diagram.png')} style={{ width: "100%" }}/>
@@ -77,7 +86,7 @@ export const StoryWhyWhyLeSS: React.FC = () => {
 			</AbsoluteFill>
 			<AbsoluteFill style={{ left: '90%', top: '2%', width: '10%', height: '20%'}}>
 				<OddeLogo />
-				<FlipCoin speed={2} interval={4} shift={1} >
+				<FlipCoin speed={2} interval={20} shift={1} >
 					<OddeLogoInner />
 				</FlipCoin>
 			</AbsoluteFill>
