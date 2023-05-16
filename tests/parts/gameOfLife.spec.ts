53a3b11a97cc010f691b28669527703c43c2b2b2
Terry Yin <terry.yinzhe@gmail.com>
2023-05-16 10:21:57 +0800

    neighbour

--- tests/parts/gameOfLife.spec.ts
+++ tests/parts/gameOfLife.spec.ts
@@ -1,15 +1,35 @@
 class Cell {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private x: number, private y: number) { }
+
+  neighbourCells(): Cell[] {
+    return [
+      new Cell(this.x - 1, this.y - 1),
+      new Cell(this.x - 1, this.y),
+      new Cell(this.x - 1, this.y + 1),
+      new Cell(this.x, this.y - 1),
+      new Cell(this.x, this.y + 1),
+      new Cell(this.x + 1, this.y - 1),
+      new Cell(this.x + 1, this.y),
+      new Cell(this.x + 1, this.y + 1),
+    ];
+  }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function gameOfLifeSurvivors(aliveCells: Cell[]): Cell[] {
+  if(aliveCells.length === 3) return aliveCells;
   return [];
 }
 
 describe('An alive cell in Game Of Life', () => {
-  const subject = new Cell();
+  const subject = new Cell(2, 3);
 
   it('dies with no alive neighbours', () => {
     expect(gameOfLifeSurvivors([subject])).not.toContain(subject);
   });
+
+  it('survives with 2 alive neighbours', () => {
+    expect(gameOfLifeSurvivors([subject, ...subject.neighbourCells().slice(0, 2)])).toContain(subject);
+  });
+
 });
