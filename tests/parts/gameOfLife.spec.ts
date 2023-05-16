86492bf55ac458608be7ac37b741dc88e70de6f4
Terry Yin <terry.yinzhe@gmail.com>
2023-05-16 11:17:04 +0800

    having

--- tests/parts/gameOfLife.spec.ts
+++ tests/parts/gameOfLife.spec.ts
@@ -18,12 +18,11 @@ class Cell {
 }
 
 function gameOfLifeSurvivors(aliveCells: Cell[]): Cell[] {
-  return aliveCells.filter((cell) => {
-    const aliveNeighbours = _.intersectionWith(aliveCells, cell.neighbourCells(), _.isEqual);
-    console.log(aliveNeighbours)
-    return aliveNeighbours.length === 2 || aliveNeighbours.length === 3;
-  }
-  );
+  const aliveNeighbours = (cell: Cell): Cell[] => _.intersectionWith(aliveCells, cell.neighbourCells(), _.isEqual);
+  const nearbyCells = [...new Set(aliveCells.flatMap((cell) => cell.neighbourCells()))];
+  const having3Neighbours = nearbyCells.filter((cell) => aliveNeighbours(cell).length === 3);
+  const having2Neighbours = aliveCells.filter((cell) => aliveNeighbours(cell).length === 2);
+  return [...new Set([...having3Neighbours, ...having2Neighbours])];
 }
 
 describe('An alive cell in Game Of Life', () => {
@@ -31,21 +30,25 @@ describe('An alive cell in Game Of Life', () => {
   const aliveNeighbours = (n: number): Cell[] => subject.neighbourCells().slice(0, n);
 
   it('dies with no alive neighbours', () => {
-    expect(gameOfLifeSurvivors([subject])).not.toContain(subject);
+    expect(gameOfLifeSurvivors([subject])).not.toContainEqual(subject);
   });
 
   it('dies if there are only remote neighbours', () => {
     const remoteNeighbour1 = subject.neighbourCells()[0].neighbourCells()[0];
     const remoteNeighbour2 = subject.neighbourCells()[1].neighbourCells()[1];
-    expect(gameOfLifeSurvivors([subject, remoteNeighbour1, remoteNeighbour2])).not.toContain(subject);
+    expect(gameOfLifeSurvivors([subject, remoteNeighbour1, remoteNeighbour2])).not.toContainEqual(subject);
   });
 
   it('survives with 2 alive neighbours', () => {
-    expect(gameOfLifeSurvivors([subject, ...aliveNeighbours(2)])).toContain(subject);
+    expect(gameOfLifeSurvivors([subject, ...aliveNeighbours(2)])).toContainEqual(subject);
   });
 
   it('survives with 3 alive neighbours', () => {
-    expect(gameOfLifeSurvivors([subject, ...aliveNeighbours(3)])).toContain(subject);
+    expect(gameOfLifeSurvivors([subject, ...aliveNeighbours(3)])).toContainEqual(subject);
+  });
+
+  it('revives with 3 alive neighbours', () => {
+    expect(gameOfLifeSurvivors([...aliveNeighbours(3)])).toContainEqual(subject);
   });
 
 });
