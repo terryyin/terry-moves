06e4680fd87e6d7d720e88e2f8b28c8d3fe4f9cd
Terry Yin <terry.yinzhe@gmail.com>
2023-06-03 11:31:00 +0800

    rise

--- src/StoryProductDeveloper.tsx
+++ src/StoryProductDeveloper.tsx
@@ -45,7 +45,9 @@ export const productDeveloperSubtitles: Subtitle[] = [
 		{ actor: 'product', actionType: 'rotate and rise', duration: 1, value: 4 },
 		{ actor: 'product', actionType: '3d rotate', endingTimeAdjustment: 4, totalRotation: [0, 360 + 15, 0] },
 	] },
-	{ leadingBlank: 1, duration: 5, text: 'But traditionally, businesses have been focused on their core domain, not software. ' },
+	{ leadingBlank: 1, duration: 5, text: 'But traditionally, businesses have been focused on their core domain, not software. ', actions: [
+		{ actor: 'business person', actionType: 'appear', startDuration: 1 },
+	] },
 	{ leadingBlank: 1, duration: 4, text: 'In the past, developers were commonly external.', actions:[
 		{ actor: 'stage', actionType: 'move', duration: 1, absolutePosition: [350, 0] },
 		{ actor: "camera", actionType: "move", duration: 1, absolutePosition: [-1, 0, 5] },
@@ -56,9 +58,9 @@ export const productDeveloperSubtitles: Subtitle[] = [
 		{ actor: 'cylinder', actionType: 'scale', outputRange: [0, 1], duration: 3, offset: 2 },
 		{ actor: 'hex', actionType: 'scale', outputRange: [0, 1], duration: 3, offset: 2 },
 		{ actor: 'month1', actionType: 'appear', startDuration: 1, offset: 2 },
-		{ actor: 'month2', actionType: 'appear', startDuration: 1, offset: 3 },
-		{ actor: 'month3', actionType: 'appear', startDuration: 1, offset: 4 },
-		{ actor: 'month4', actionType: 'appear', startDuration: 1, offset: 5 },
+		{ actor: 'month2', actionType: 'appear', startDuration: 1, offset: 4 },
+		{ actor: 'month3', actionType: 'appear', startDuration: 1, offset: 5 },
+		{ actor: 'month4', actionType: 'appear', startDuration: 1, offset: 6 },
 	] },
 	{ leadingBlank: 1, duration: 6, text: 'Integration was viewed as a simple phase, yet it consistently led to unexpected complications.', actions: [
 		{ actor: 'hex', actionType: '3d rotate', totalRotation: [-120, 0, -90], endingTimeAdjustment: 3, offset: 2 },
@@ -75,9 +77,9 @@ export const productDeveloperSubtitles: Subtitle[] = [
 	{ leadingBlank: 1, duration: 7, text: 'However, times are changing. More businesses are adopting a product-centric strategy, aiming to grow their competitive edge.', actions: [
 		{ actor: 'hex', actionType: 'disappear', startDuration: 0.1 },
 		{ actor: 'cylinder', actionType: 'disappear', startDuration: 0.1 },
-		{ actor: 'stage', actionType: 'move', duration: 2, absolutePosition: [0, 0] },
-		{ actor: 'product', actionType: '3d rotate', endingTimeAdjustment: 1, totalRotation: [0, 360 -10, 0], offset: 0 },
-		{ actor: 'product', actionType: 'move', duration: 2, absolutePosition: [0, 0.9, 0] },
+		{ actor: 'stage', actionType: 'move', duration: 2, absolutePosition: [0, 0], },
+		{ actor: 'product', actionType: '3d rotate', endingTimeAdjustment: 1, totalRotation: [0, 360 -10, 0], offset: 3 },
+		{ actor: 'product', actionType: 'move', duration: 2, absolutePosition: [0, 0.9, 0], offset: 3 },
 
 	]},
 	{ leadingBlank: 1, duration: 5, text: 'This has sparked the rise of Product Developers.', actions: [
@@ -98,11 +100,11 @@ export const productDeveloperSubtitles: Subtitle[] = [
 		{ actor: "pd team1", actionType: "connect to", startDuration: 1, endingTimeAdjustment: 3, target: "donor edge", bentLevel: -30, strokeWidth: 2, offset: 3},
 		{ actor: "pd team2", actionType: "connect to", startDuration: 1, endingTimeAdjustment: 3, target: "donor edge", bentLevel: -30, strokeWidth: 2, offset: 3},
 	]},
-	{ leadingBlank: 1, duration: 6, text: `a term for those who've convinced the Product Owner to invest in their product hypothesis.`, actions: [
-		{ actor: 'mask', actionType: 'appear', startDuration: 1, endingTimeAdjustment: 6 },
-		{ actor: 'connectors', actionType: 'disappear', startDuration: 1, endingTimeAdjustment: 6 },
-		{ actor: 'donor explainer', actionType: 'appear', startDuration: 1, endingTimeAdjustment: 6 },
-		{ actor: 'aha', actionType: 'appear', startDuration: 1, endingTimeAdjustment: 4, offset: 2 },
+	{ leadingBlank: 1, duration: 8, text: `a term for those who've convinced the Product Owner to invest in their product hypothesis.`, actions: [
+		{ actor: 'mask', actionType: 'appear', startDuration: 1, endingTimeAdjustment: 8 },
+		{ actor: 'connectors', actionType: 'disappear', startDuration: 1, endingTimeAdjustment: 8 },
+		{ actor: 'donor explainer', actionType: 'appear', startDuration: 1, endingTimeAdjustment: 8 },
+		{ actor: 'aha', actionType: 'appear', startDuration: 1, endingTimeAdjustment: 5, offset: 3 },
 
 	]},
 	{ leadingBlank: 1, duration: 8, text: `These donors could be customers, users, product managers, or even fellow developers, etc.	`, actions: [
@@ -120,10 +122,9 @@ export const productDeveloperSubtitles: Subtitle[] = [
 		{ actor: 'donor cap', actionType: 'move', duration: 1, absolutePosition: [-1330, 1080, 0], offset: 6 },
 		{ actor: 'donor cap', actionType: 'scale', duration: 1, outputRange: [0.4, 0.3], offset: 6 },
 		{ actor: 'donor cap', actionType: '3d rotate', endingTimeAdjustment: 1, totalRotation: [0, 0, 20], offset: 6 },
-
-
 	]},
-	{ leadingBlank: 1, duration: 6, text: 'Product Developers operate with a customer-centric focus, constantly checking in with reality and seeking early feedback.', actions: [
+	{ leadingBlank: 1, duration: 3, text: 'Regardless who is the requirement donor, '},
+	{ leadingBlank: 0, duration: 6, text: 'Product Developers operate with a customer-centric focus, constantly checking in with reality and seeking early feedback.', actions: [
 		{ actor: 'donor cap', actionType: 'disappear', startDuration: 2},
 		{ actor: 'gift product', actionType: 'appear', startDuration: 1, endDuration: 4, endingTimeAdjustment: 6 },
 		{ actor: 'gift product', actionType: 'move', duration: 4, absolutePosition: [350, 0, 0], offset: 2 },
@@ -151,7 +152,8 @@ export const productDeveloperSubtitles: Subtitle[] = [
 
 const titleText = `
 # Product Developers:
-Who are they and why are they on the rise?`
+
+### Who are they and why are they on the rise?`
 
 const url = staticFile('assets/shape_sorting_box/scene.gltf')
 useLoader.preload(GLTFLoader, url);
@@ -251,7 +253,7 @@ export const StoryProductDeveloper: React.FC = () => {
 					<AnimationEffect actor="title" style={{ left: '0%', top: '35%', width: '100%', height: '65%' }} >
 						<EdgeWaver actor="title-shadow" amplitudePercentageOfHeight={6} frequency={1.2} style={{ backgroundColor: 'rgba(253, 250, 88, 0.8)' }}  />
 						<EdgeWaver actor="title" amplitudePercentageOfHeight={6} frequency={1.2} style={{backgroundColor: 'rgba(0, 114, 160, 0.8)' }}  >
-						<Markdown actor="conclusion" style={{ paddingTop: '50px', paddingLeft: '10px', fontSize: "x-large"}}
+						<Markdown actor="" style={{ paddingTop: '50px', paddingLeft: '10px', fontSize: "x-large"}}
 							md={titleText}
 						/>
 					</EdgeWaver>
@@ -290,7 +292,7 @@ Anyone who has successfully convinced the Product Owner to invest in their produ
 				</AnimationEffect>
 
 
-				<Subtitles />
+				<Subtitles scale={0.6} />
 			</AbsoluteFill>
 
     </Story>
